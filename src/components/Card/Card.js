ff14755a5f96ee958afc5b2e602072149aedc8fd
Venkatraman <ramsunvtech@gmail.com>
2020-04-18 20:29:35 +0800

    feat(page): Column List with Cards

---
+++ src/components/Card/Card.js
@@ -0,0 +1,18 @@
+import BaseComponent from '../BaseComponent/BaseComponent.js';
+
+class Card extends BaseComponent {
+    constructor() {
+        super();
+    }
+
+    render() {
+        const title = this.getAttribute('title');
+
+        this.innerHTML = `
+            <li>${title}</li>
+        `;
+    }
+  }
+  
+  export default Card;
+  
