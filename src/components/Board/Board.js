3ae2bc91b5074d71edae0b6b594682f4d2471b50
Venkatraman <ramsunvtech@gmail.com>
2020-04-18 20:28:56 +0800

    feat(base): Card and Column Components Setup

---
+++ src/components/Board/Board.js
@@ -0,0 +1,52 @@
+import BaseComponent from '../BaseComponent/BaseComponent.js';
+
+const columnList = [
+    {
+      "id": "1",
+      "title": "To do",
+      "boardId": "1"
+    },
+    {
+      "id": "2",
+      "title": "Doing",
+      "boardId": "1"
+    },
+    {
+      "id": "3",
+      "title": "Done",
+      "boardId": "1"
+    }
+];
+
+class Board extends BaseComponent {
+    constructor() {
+        super();
+    }
+
+    getColumnList() {
+        //@todo: Column List API Goes here.
+        const columns = columnList.map(column => {
+            const {
+                id,
+                title,
+            } = column;
+            return `
+                <app-column id="${id}" title="${title}"></app-column>
+            `;
+        });
+
+        return columns.join('\n');
+    }
+
+    render() {
+        this.innerHTML = `
+            <section class="lists-container">
+                ${this.getColumnList()}
+                <button class="add-list-btn btn">Add a list</button>
+            </section>
+        `;
+    }
+  }
+  
+  export default Board;
+  
